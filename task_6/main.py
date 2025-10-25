1d9da6bdedc482a2bd5940b917a47a1b0aa2624e
faloger <gtllop098@gmail.com>
2025-10-25 15:57:53 +0300

    Update main.py

--- task_6/main.py
+++ task_6/main.py
@@ -1,7 +1,10 @@
 #Доброва Анна
-file = open("text.txt", "r")#открываем файл для чтения
-data = file.readlines()#считываем все строки в список
-new_file = open("output.txt", "w")#Открываем файл для записи
-for line in data:#перебираем строки
-    reversed_line = line.rstrip()[::-1]#Удаляем перенос строки и переворачиваем
-    new_file.write(reversed_line + "\n")#Записываем перевёрнутую строку
+file_ = "S:/students/GR_88/Доброва Анна/ИПО/text.txt" #путь файла
+with open(file_, 'r', encoding ='utf-8') as file:#открываем файл для чтения
+    data = file.readlines()#чтение файла
+new_file = "S:/students/GR_88/Доброва Анна/ИПО/output.txt" #путь файла
+with open(new_file, "w",  encoding ='utf-8') as file:#Открываем файл для записи
+    for line in data:#перебираем строки
+        reversed_line = line.rstrip()[::-1]#Удаляем перенос строки и переворачиваем
+        file.write(reversed_line + "\n")#Записываем перевёрнутую строку
+
